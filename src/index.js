7ff4ac36728ec4043ca76a33c941b1103cb2e8b5
Sobokar <eugenabond@gmail.com>
2024-03-18 09:57:37 +0200

    Injection of current weather info
    Current week day, time, conditions, wind and humidity data added

--- src/index.js
+++ src/index.js
@@ -12,18 +12,44 @@ function searchCity(city) {
   let apiKey = "9400caa5odc3060fcf5ba1d37t3aaf2c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   console.log(apiUrl);
-  axios.get(apiUrl).then(updateTemperature);
+  axios.get(apiUrl).then(updateweatherInfo);
 }
 
-function updateTemperature(response) {
+function updateweatherInfo(response) {
   let temperatureElement = document.querySelector("#weather-app-temperature");
   let currentTemperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(currentTemperature);
-
   let cityElement = document.querySelector("#app-city");
+  let conditionsElement = document.querySelector("#conditions");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-  console.log(response.data.city);
+  temperatureElement.innerHTML = Math.round(currentTemperature);
   cityElement.innerHTML = response.data.city;
+  conditionsElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  timeElement.innerHTML = formatDate(date);
 }
 
-searchCity("Kyiv");
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
+searchCity();

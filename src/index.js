ba559d9863572901cbe201634b91150700b84986
Sobokar <eugenabond@gmail.com>
2024-03-25 14:13:04 +0200

    added actual forecast info via API

--- src/index.js
+++ src/index.js
@@ -64,25 +64,36 @@ function getForecast(city) {
   axios(apiUrl).then(displayForecast);
 }
 
-function displayForecast(response) {
-  console.log(response.data);
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 
-  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      ` 
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        ` 
   <div class="weather-forecast-date">
-    <div class="weather-forcast-day">${day}</div>
-    <div class="weather-forecast-icon">☀️</div>
+    <div class="weather-forecast-day">${formatDay(day.time)}</div>
+  
+    <img src= "${day.condition.icon_url}" class="weather-forecast-icon" />
+   
             
         <div class="weather-forecast-temperature">
-                      <span class="weather-forecast-temperature-max">26°</span>
-                      <span class="weather-forecast-temperature-min">20°</span>
+                      <span class="weather-forecast-temperature-max">${Math.round(
+                        day.temperature.maximum
+                      )}°</span>
+                      <span class="weather-forecast-temperature-min">${Math.round(
+                        day.temperature.minimum
+                      )}°</span>
         </div>
  </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
